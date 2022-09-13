e2849fe38efcabdc81aa5c4ff8c3c793f6caefe0
Redarokim Ikongha <43537915+rikongha@users.noreply.github.com>
2022-09-13 14:01:00 +0000

    feat: implement script to run service

--- run_docker.sh
+++ run_docker.sh
@@ -2,11 +2,9 @@
 
 ## Complete the following steps to get Docker running locally
 
-# Step 1:
-# Build image and add a descriptive tag
+docker build --tag=micros-k8s-api .
 
-# Step 2: 
-# List docker images
+docker image ls
 
-# Step 3: 
 # Run flask app
+docker run -p 8000:80 micros-k8s-api
