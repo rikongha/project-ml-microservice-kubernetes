8725d3f540543fea33bd66a8411f8d8125af7f62
Redarokim Ikongha <43537915+rikongha@users.noreply.github.com>
2022-09-13 14:01:25 +0000

    feat: implement push to registry

--- upload_docker.sh
+++ upload_docker.sh
@@ -2,14 +2,21 @@
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
+dockerpath="rikongha/micros-k8s-api:v1.0.0"
 
-# Step 1:
-# Create dockerpath
-# dockerpath=<your docker ID/path>
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag micros-k8s-api $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
-# Push image to a docker repository
+docker push $dockerpath
+
+
+
+
+# TODO: improve versioning
+# REV_TAG=$(git log -1 --pretty=format:%h)
+# docker build -t micros-k8s-api:$REV_TAG .
