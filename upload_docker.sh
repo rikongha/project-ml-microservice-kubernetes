2347afe5867a80caacfeb8fc687ebe98e3ead804
Redarokim Ikongha <43537915+rikongha@users.noreply.github.com>
2022-09-14 03:15:26 +0000

    docs: correctly output docs and minor formatting

--- upload_docker.sh
+++ upload_docker.sh
@@ -4,19 +4,11 @@
 # Assumes that an image is built via `run_docker.sh`
 dockerpath="rikongha/micros-k8s-api:v1.0.0"
 
-
-# Step 2:  
 # Authenticate & tag
 docker login
 docker tag micros-k8s-api $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
-# Step 3:
 docker push $dockerpath
 
-
-
-
 # TODO: improve versioning
-# REV_TAG=$(git log -1 --pretty=format:%h)
-# docker build -t micros-k8s-api:$REV_TAG .
