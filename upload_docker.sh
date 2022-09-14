1cd47974d41efc014880c7b94e897cdf1b482e07
Redarokim Ikongha <43537915+rikongha@users.noreply.github.com>
2022-09-14 11:05:09 +0000

    fix: updates version number for new image release

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
 
-# Assumes that an image is built via `run_docker.sh`
-dockerpath="rikongha/micros-k8s-api:v1.0.0"
+tag=v1.0.1
+dockerpath="rikongha/micros-k8s-api"
+fullpath=$dockerpath:$tag
 
 # Authenticate & tag
 docker login
-docker tag micros-k8s-api $dockerpath
+docker tag micros-k8s-api $fullpath
 echo "Docker ID and Image: $dockerpath"
 
-docker push $dockerpath
-
-# TODO: improve versioning
+docker push $fullpath
